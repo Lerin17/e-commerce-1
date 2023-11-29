96caeecda830c0850e77dfe11d1617896122b616
Lerin17 <lerinowoade17@gmail.com>
2023-11-29 11:57:25 +0100

    favourite Button UI update #1

--- src/pages/allItempage.js
+++ src/pages/allItempage.js
@@ -139,9 +139,18 @@ const FavouriteBtn = (props) => {
 
 
   return (
-    <IconButton onClick={()=>updateUserFavourite(props.id)} className= {`${favIDs.includes(props.id)? 'text-red-600':'text-white'}`} >
-    <FavoriteBorderOutlined/>
-     </IconButton>
+    <div className="pl-2 mb-1 cursor-pointer" onClick={()=>updateUserFavourite(props.id)}>
+        {favIDs.includes(props.id)?
+        <svg className= {`${favIDs.includes(props.id)? 'text-red-600 fill-current':''}`}  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/></svg>
+        :<svg className="text-red-600 fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z"/></svg>}
+          
+    </div>
+  
+    // <IconButton onClick={()=>updateUserFavourite(props.id)} className= {`${favIDs.includes(props.id)? 'text-red-600 bg-red-600':'text-white'}`} >
+    // <FavoriteBorderOutlined
+    // className="fill-current"
+    // />
+    //  </IconButton>
   )
 }
 
@@ -338,7 +347,11 @@ console.log(allProducts)
        <div className="flex justify-between" >
         <div className="w-full border-r-4 border-b-4 lg:border-b-2 lg:border-r-2  border-gray-500" >
           <div className="font-bold uppercase border-4 border-gray-300  font-headers flex pt-1 text-lg bg-blue-600 hover:bg-black transition-all text-white items-center px-2" >
-              {item.name}
+
+            <div>
+            {item.name}
+            </div>
+            
 
               <div id={item._id} productid = {item._id}     className="self-center ">
               <FavouriteBtn
